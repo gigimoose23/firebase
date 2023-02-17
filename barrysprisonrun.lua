dbe6837d4690366bba05f9bb69e7d0f4007d28ad
gigimoose23 <88978318+gigimoose23@users.noreply.github.com>
2023-02-17 19:50:56 +0000

    Update barrysprisonrun.lua

--- barrysprisonrun.lua
+++ barrysprisonrun.lua
@@ -1,5 +1,9 @@
 local userInputService = game:GetService("UserInputService")
 
 if userInputService.TouchEnabled then
-    
+    print("Compatibility version loading")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/gigimoose23/firebase/main/barrygui_tabletcompatibiliity.lua", true))()
+else
+    print("Computer Version loading")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/gigimoose23/firebase/main/barrygui.lua", true))()
 end
